334019ef3ba6a6dbf22d3581ff20513a5dde5e92
ValentinaRodRo <valentinarodro@unisabana.edu.co>
2025-11-12 15:06:15 -0500

    creación y función de transición (connect4.py)

---
+++ connect4.py
@@ -0,0 +1,61 @@
+class Connect4:
+    FILAS, COLUMNAS = 6, 7
+
+    def __init__(self):
+        #Tablero vacio
+        self.tablero = np.zeros((self.FILAS, self.COLUMNAS), dtype =int)
+
+        self.turno = 1 #1 es el turno rojo, -1 el turno amarillo
+        self.final = False #Juego terminado (gana uno o empate)
+        self.ganador = None
+
+    def col_libre(self):
+        return [col for col in range(self.COLUMNAS) if self.tablero[0, col]==0]
+    
+    def step(self, accion):
+        #aplica una accion y devuelve el nuevo estado (funcion de transicion)
+
+        fila = max(fil for fil in range (self.FILAS) if self.tablero[fil, accion]==0) #busca la ultima fila vacia en la columna que elige el jugador (accion)
+        self.tablero[fila, accion] = self.turno
+
+        if self.check_win (fila, accion):
+            self.final = True
+            self.ganador = self.turno 
+
+        elif not self.col_libre():
+            self.final=True
+            self.ganador = 0 
+        else:
+            self.turno = self.turno * -1 
+        return self.final, self.ganador
+    
+    def check_win (self, fila, col):
+        jugador = self.tablero[fila, col]
+
+        direcciones = [(1,0), (0,1), (1,1), (1, -1)]
+
+        for df, dc in direcciones:
+            contador = 1
+
+            #hacia adelante
+            f = fila + df
+            c = col + dc
+
+            while 0 <= f < self.FILAS and 0 <= c < self.COLUMNAS and self.tablero[f, c] == jugador:
+                contador += 1
+                f += df
+                c += dc
+
+            #hacia atras
+            f = fila - df
+            c = col - dc
+
+            while 0 <= f < self.FILAS and 0 <= c < self.COLUMNAS and self.tablero[f, c] == jugador:
+                contador += 1
+                f-= df
+                c -= dc
+
+            if contador >= 4:
+                return True
+        
+        return False
